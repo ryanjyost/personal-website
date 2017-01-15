252c4f715b722c6f3025bf130b4a21195444eb38
Ryan Yost <ryost217@gmail.com>
2017-01-15 12:26:55 -0600

    mobile menu grid/tic-tac-toe bloard

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var sass = require('node-sass');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
