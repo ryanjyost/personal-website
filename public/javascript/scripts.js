d0f730f1b1b34bf927aff2711d1476f1246ac2fb
Ryan Yost <ryost217@gmail.com>
2017-01-15 21:13:50 -0600

    experimenting with animation on mobile

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 
+	$('#hi-im-ryan').delay(1000).slideUp(400);
+	$('#play-tic-tac-toe').delay(1000).slideDown(400);
 
+	$('.menu-item').delay(1050).fadeTo(700, 1, function(){
 
-	$('.menu-item').delay(800).fadeTo(800, 1, function(){
-		$('#hi-im-ryan').slideUp('slow');
-		$('#play-tic-tac-toe').slideDown('slow');
 	});
+
+
 
 
 
