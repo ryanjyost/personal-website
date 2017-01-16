63c2f9ce56face4869496469677db3dab431648b
Ryan Yost <ryost217@gmail.com>
2017-01-15 21:40:52 -0600

    want to play? buttons

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
 
-	$('#hi-im-ryan').delay(1000).slideUp(400);
-	$('#play-tic-tac-toe').delay(1000).slideDown(400);
+	$('#me-intro-1').delay(1000).slideUp(400);
+	$('#me-intro-2').delay(1000).slideDown(400);
 
-	$('.menu-item').delay(1050).fadeTo(700, 1, function(){
-
-	});
+	$('.menu-item').delay(1050).fadeTo(700, 1);
+	$('#icons8').delay(1050).fadeTo(700, 1);
 
 
 
