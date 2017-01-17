70a24d9fdb17815ff34cfe376e4e70552a0a1276
Ryan Yost <ryost217@gmail.com>
2017-01-16 19:53:54 -0600

    add play/not play buttons

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
 	$('#me-intro-1').delay(1000).slideUp(400);
 	$('#me-intro-2').delay(1000).slideDown(400);
 
-	$('.menu-item').delay(1050).fadeTo(700, 1);
+	$('.menu-item').delay(1050).fadeTo(700, .3);
 	$('#icons8').delay(1050).fadeTo(700, 1);
-	$('.play-btn').delay(1350).fadeTo(700, 1);
+	$('.play-btn-cont').delay(1350).fadeTo(700, 1);
 
 
 
