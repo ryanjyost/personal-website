8e1962339673a16815c16fc308fc9129794c83a8
Ryan Yost <ryost217@gmail.com>
2017-01-16 20:25:18 -0600

    disable scroll on intro

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
 
+	//=====================================================
+	// Initial Page Load
+	//=====================================================
+	$('body').addClass('disable-scroll');
 	$('#me-intro-1').delay(1000).slideUp(400);
 	$('#me-intro-2').delay(1000).slideDown(400);
 
 	$('.menu-item').delay(1050).fadeTo(700, .3);
 	$('#icons8').delay(1050).fadeTo(700, 1);
-	$('.play-btn-cont').delay(1350).fadeTo(700, 1);
+	$('.play-btn-cont').delay(1250).fadeTo(700, .8);
+
+	let game = false;
+
+	//=====================================================
+	// Play Tic-Tac-Toe
+	//=====================================================
+
+	$('#thumbs-up-icon').click(function(){
+
+	})
 
 
 
